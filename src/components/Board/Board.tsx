4019d0173e09a75d881ce654d4a85e268faa0f6b
Marina <2175567+AmbarPHP@users.noreply.github.com>
2022-11-21 14:17:00 -0700

    Fix take out modal

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,22 +1,23 @@
 import Modal from '@mui/material/Modal';
 import React from 'react'
+import Column from './Column';
 
 function Board() {
   return (
-    <Modal
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description"
-  >
+    //todo, cuando cambias en el menu de pagina--> vamos a dashboard==> que abre board 
+    //todo: leemos la cantidad de columnas y las dibujamos
     <div className={`Board`}>
       <p className='Board__txt'>This board is empty. Create a new column to get started.</p>
       <div className='Board--noItem__btn'>
-        <button
-        >
-          &nbsp; + Create New Board &nbsp;
+        
+          <Column></Column>
+        
+        <button>
+          &nbsp; + Create New Column;
         </button>
       </div>
     </div>
-    </Modal>
+    
   );
 
 }
